b0bc9eadf4f120735b85887b46577774d052c4c9
hello7642 <aanchalbandi236@gmail.com>
2025-03-18 16:42:04 +0000

    Add files via upload

--- satelliteconnect.py
+++ satelliteconnect.py
@@ -4,6 +4,9 @@ from time import time
 WIDTH=500
 HEIGHT=500
 totalsat=10
+game_over=False
+game_win=False
+score=0
 sat=[]
 for i in range(10):
     img=Actor("satellite")
@@ -12,11 +15,18 @@ for i in range(10):
 start_time=time()
 total_time=0
 def draw():
+    global totaltime
     screen.blit("ospace",(0,0))
     if currentsat<totalsat:
         totaltime=time()-start_time
         screen.draw.text(str(round(totaltime)),(450,20),fontsize=30)
-
+    else:
+        screen.draw.text(str(round(totaltime)),(450,20),fontsize=30)
+    if game_over:
+        screen.fill("white")
+        screen.draw.text("Time is up!Your final score is:"+str(score),color="black",midtop=(250,250),fontsize=40)
+    if game_win:
+        screen.draw.text("You have won the game!",color="white",midtop=(250,250),fontsize=40)
     num=1
     for i in sat:
         i.draw()
@@ -28,16 +38,29 @@ def update():
     pass
 currentsat=0
 line=[]
+def over():
+    global game_win
+    global game_over
+    if len(line)==9:
+        game_win=True
+    else:
+         game_over=True
+
+clock.schedule(over,20)
+
 def on_mouse_down(pos):
+    global score
     global currentsat,line,totalsat
     if currentsat<totalsat:
         if  sat[currentsat].collidepoint(pos):
             if currentsat:
               line.append((sat[currentsat-1].pos,sat[currentsat].pos))
             currentsat+=1
+            score+=1
         else:
             line=[]
             currentsat=0
+            score=0
 
 
 
