9cbe3451942c1630d7d7b4a4ab6ca8c1c04b382f
hello7642 <aanchalbandi236@gmail.com>
2025-03-25 16:56:31 +0000

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,13 @@
+import pgzrun
+WIDTH=750
+HEIGHT=750
+TITLE="General knowledge contest"
+scrollerbox=Rect(0,0,750,100)
+
+
+
+
+def draw():
+    screen.fill("black")
+    screen.draw.filled_rect(scrollerbox,"cyan")
+pgzrun.go()    
